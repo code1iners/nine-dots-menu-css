9f242ce3c020fb3128d5bbdc5d925f438e641972
Codeliner <codeliner96@gmail.com>
2022-02-05 12:46:18 +0900

    Code refactoring and add README file.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nine Dots Menu.
+
+This is a simple menu icon with animations.
+
+[Go check it out!](https://code1iners.github.io/nine-dots-menu-css/)
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+// Navigation box.
 const nav = document.querySelector(".nav");
 nav.addEventListener("click", ({ target }) => {
-  if (target === nav) {
-    nav.classList.toggle("active");
-  }
+  if (target === nav) nav.classList.toggle("active");
 });
 
+// Close button.
 const closeButton = document.querySelector(".close-button");
-closeButton.addEventListener("click", (e) => {
-  nav.classList.toggle("active");
-});
+closeButton.addEventListener("click", () => nav.classList.toggle("active"));
